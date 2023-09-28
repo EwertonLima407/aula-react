6b22743da13bcc48d76b8c77a91b5b2c6d2fa336
Ewerton Lima <ewerton_rlima@hotmail.com>
2023-09-28 16:20:55 -0300

    feat: login page useRef

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,27 +1,54 @@
-import { useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleEntrar = () => {
+  useEffect(() => {
+    if (window.confirm('Você é homem?')) {
+      console.log('Homem')
+    } else {
+      console.log('Mulher')
+
+    }
+
+  }, [])
+
+  useEffect(() => {
+    console.log(email)
+  }, [email])
+
+
+  useEffect(() => {
+    console.log(password)
+  }, [password])
+
+  const emailLength = useMemo(() => {
+    return email.length * 1000
+  }, [email.length]);
+
+  const handleEntrar = useCallback(() => {
     console.log(email)
     console.log(password)
 
-  }
+  }, [email, password])
+
   return (
     <div>
       <form>
+        <p>Quantidade de letra de email: {emailLength}   </p>
         <label>
           <span>Email</span>
-          <input value={email} onChange={e => setEmail(e.target.value)} />
+          <input value={email} onChange={e => setEmail(e.target.value)}
+            onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined} />
         </label>
 
         <label>
           <span>Senha</span>
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input type="password" ref={inputPasswordRef} value={password} onChange={e => setPassword(e.target.value)} />
         </label>
 
         <button type="button" onClick={handleEntrar}>
