ad5ffd7a588b926d7f5829f74a874b0607158e31
Ewerton Lima <ewerton_rlima@hotmail.com>
2023-10-06 16:17:12 -0300

    feat: refactoring for search from API

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useState } from "react";
 
-interface IListemItem {
+interface ITarefa {
+  id: number;
   title: string;
-  isSelected: boolean;
+  isCompleted: boolean;
 }
 
 export const Dashboard = () => {
-  const [lista, setLista] = useState<IListemItem[]>([]);
+  const [lista, setLista] = useState<ITarefa[]>([]);
 
   const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
     if (e.key === 'Enter') {
@@ -22,7 +23,8 @@ export const Dashboard = () => {
 
         return [...oldLista, {
           title: value,
-          isSelected: false,
+          isCompleted: false,
+          id: oldLista.length
         }]
       })
     }
@@ -36,22 +38,22 @@ export const Dashboard = () => {
 
       <input onKeyDown={handleInputKeyDown} />
 
-      <p>{lista.filter((listItem) => listItem.isSelected).length}</p>
+      <p>{lista.filter((listItem) => listItem.isCompleted).length}</p>
       <ul>
         {lista.map((listItem) => {
-          return <li key={listItem.title}>
+          return <li key={listItem.id}>
             <input
               type="checkbox"
-              checked={listItem.isSelected}
+              checked={listItem.isCompleted}
               onChange={() => {
                 setLista(oldLista => {
                   return oldLista.map(oldListItem => {
-                    const newIsSelected = oldListItem.title === listItem.title
-                      ? !oldListItem.isSelected
-                      : oldListItem.isSelected
+                    const newIsCompleted = oldListItem.title === listItem.title
+                      ? !oldListItem.isCompleted
+                      : oldListItem.isCompleted
                     return {
                       ...oldListItem,
-                      isSelected: newIsSelected
+                      isCompleted: newIsCompleted
                     }
                   })
                 })
